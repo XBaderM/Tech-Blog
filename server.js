8a13a547407efb0195b706a075737ea03cea9efc
Bader Munir <bader.munir18@googlemail.com>
2022-08-28 20:18:26 +0100

    add get home routes

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
+const routes = require('./controllers')
 const hbs = exphbs.create({
     
 });
@@ -12,12 +13,14 @@ const PORT = process.env.PORT || 3001;
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(express.json());
 app.use(express.urlencoded({
     extended: true
 }));
 
+app.use(routes);
 //sequelize.sync();
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
